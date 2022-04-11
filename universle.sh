1586a8c5c0d916e5aa86043744f3b58f001c0b4a
iownbey <iownbey@hotmail.com>
2022-04-10 20:20:33 -0400

    Added some comments

--- universle.sh
+++ universle.sh
@@ -29,10 +29,12 @@ printf -- "-- Welcome to Universle! --\n\nStart guessing:\n"
 # Guess Number
 gn=0
 guess=""
-# Main Guess Loop
+# Main Guess Loop (Continues until a correct guess)
 while [[ $guess != $word ]]; do
+	# Get input
 	read guess
 	gl=${#guess}
+	# Iterate guess counter
 	((gn++))
 
 	# Erase user input
@@ -42,7 +44,9 @@ while [[ $guess != $word ]]; do
 
 	# First Pass for Removing Green Letters from Yellow Letter Check Variable
 	for (( i=0; i<$gl; i++ )); do
+		# Get the ith letter
 		letter=${guess:$i:1}
+		# If they are equal, remove the letter from the letter check
 		if [[ $letter == ${word:$i:1} ]]; then
 			existcheck=${existcheck/$letter}
 		fi
@@ -50,12 +54,15 @@ while [[ $guess != $word ]]; do
 
 	# Check Guess Letters
 	for (( i=0; i<$gl; i++ )); do
+		# Get the ith letter
 		letter=${guess:$i:1}
+		# If they are equal, then print the letter as green
 		if [[ $letter == ${word:$i:1} ]]; then
 			printf $GREEN
+		# If they are not equal, but the letter is in the letter check then print it as yellow.
 		elif [[ "$existcheck" == *"$letter"* ]]; then
 			printf $YELLOW
-			# Removes the letter from a temp copy of the word so the number of yellow letters is correct
+			# Removes the letter from the temp copy of the word so the number of yellow letters is correct
 			existcheck=${existcheck/$letter}
 		fi
 		printf "${letter}${NC}"
